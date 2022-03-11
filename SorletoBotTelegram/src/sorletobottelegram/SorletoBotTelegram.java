eccf5f7eb27a3ccb9ba75177a99a23c8fa4e23ce
SorletoDomenico <82998976+SorletoDomenico@users.noreply.github.com>
2022-03-11 12:48:29 +0100

    Add files via upload

---
+++ SorletoBotTelegram/src/sorletobottelegram/SorletoBotTelegram.java
@@ -0,0 +1,29 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package sorletobottelegram;
+
+import API.Test;
+import java.io.IOException;
+
+/**
+ *
+ * @author Domen
+ */
+public class SorletoBotTelegram {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) throws IOException {
+        // TODO code application logic here
+        final String baseUrl = "https://api.telegram.org/bot";
+        final String token = "5198086511:AAEbM5tKRttQfpjyDZPmnEIXb0oogTf9Cxk/";
+        Test test = new Test();
+        
+        
+        test.sendMessage(452619758, "hello");
+        test.getUpdates("https://api.telegram.org/bot5198086511:AAEbM5tKRttQfpjyDZPmnEIXb0oogTf9Cxk/getUpdates");
+    }
+}
